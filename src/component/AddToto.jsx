bf73786fb83a54036220d2ae1263fba17d5916c6
Biddut-Roy <biddut.chandro.roy2@gmail.com>
2023-12-30 11:30:04 +0600

    complete a Redux store and reducer

---
+++ src/component/AddToto.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+
+const AddToto = () => {
+    const [input , setInput] = useState('')
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default AddToto;
